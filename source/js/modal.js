286a324a5adf9525ae4f835c042568ff547b30c8
Radiot4k <radiot4k@gmail.com>
2018-05-07 23:42:00 +0300

    Исправил появление модального окна на странице каталога

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,4 +1,4 @@
-var list = document.querySelectorAll(".good__order, .modal, .modal-overlay, .catalog-item__add-to-cart");
+var list = document.querySelectorAll(".good__order, .modal, .modal-overlay, .catalog__list");
 var OrderLink = list.item(0);
 var modal = list.item(list.length-2);
 var overlay = list.item(list.length-1);
