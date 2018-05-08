28eff0a4c9eb1fae5386e701c8b4ddcd1a9cbfd5
Radiot4k <radiot4k@gmail.com>
2018-05-08 16:59:26 +0300

    Исправил скрипт модального окна

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,20 +1,10 @@
-var list = document.querySelectorAll(".good__order, .modal, .modal-overlay");
-var OrderLink = list.item(0);
+var list = document.querySelectorAll(".good__order, .modal, .modal-overlay, .catalog-item__add-to-cart");
 var modal = list.item(list.length-2);
 var overlay = list.item(list.length-1);
-var addToCart = document.querySelectorAll(".catalog-item__add-to-cart");
 var href = document.location.pathname;
 
-if (href === "/catalog.html") {
-  for (var i = 0; i < addToCart.length; i++) {
-    addToCart.item(i).addEventListener("click", function(evt) {
-      evt.preventDefault();
-      modal.classList.add("modal-show");
-      overlay.classList.add("overlay-show");
-    });
-  }
-} else {
-  OrderLink.addEventListener("click", function(evt) {
+for (var i = 0; i < list.length - 2; i++) {
+  list.item(i).addEventListener("click", function(evt) {
     evt.preventDefault();
     modal.classList.add("modal-show");
     overlay.classList.add("overlay-show");
