0a61278529dbb7980ea337c22ba795506a01b9cb
Radiot4k <radiot4k@gmail.com>
2018-05-10 03:09:41 +0300

    Оптимизация: шрифты, html, js

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,24 @@ module.exports = function(grunt) {
           cwd: "source",
           src: [
             "fonts/**/*.{woff,woff2}",
-            "img/**",
-            "js/**"
+            "img/**"
           ],
           dest: "build"
         }]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          "build/index.html": "build/index.html",
+          "build/catalog.html": "build/catalog.html",
+          "build/form.html": "build/form.html"
+        }
       }
     },
 
@@ -61,7 +74,39 @@ module.exports = function(grunt) {
       },
       sprite: {
         files: {
-          "build/img/sprite.svg": ["source/img/icon-*.svg", "source/img/logo-htmlacademy.svg", "source/img/logo-footer.svg"]
+          "build/img/index-sprite.svg": [
+            "source/img/icon-cart.svg",
+            "source/img/icon-fb.svg",
+            "source/img/icon-feature-*.svg",
+            "source/img/icon-insta.svg",
+            "source/img/icon-interior.svg",
+            "source/img/icon-search.svg",
+            "source/img/icon-toy.svg",
+            "source/img/icon-twitter.svg",
+            "source/img/logo-footer.svg",
+            "source/img/logo-htmlacademy.svg"
+          ],
+          "build/img/catalog-sprite.svg": [
+            "source/img/icon-cart.svg",
+            "source/img/icon-fb.svg",
+            "source/img/icon-insta.svg",
+            "source/img/icon-video.svg",
+            "source/img/icon-search.svg",
+            "source/img/icon-twitter.svg",
+            "source/img/logo-footer.svg",
+            "source/img/logo-htmlacademy.svg"
+          ],
+          "build/img/form-sprite.svg": [
+            "source/img/icon-cart.svg",
+            "source/img/icon-fb.svg",
+            "source/img/icon-insta.svg",
+            "source/img/icon-mail.svg",
+            "source/img/icon-phone.svg",
+            "source/img/icon-search.svg",
+            "source/img/icon-twitter.svg",
+            "source/img/logo-footer.svg",
+            "source/img/logo-htmlacademy.svg"
+          ]
         }
       }
     },
@@ -107,6 +152,16 @@ module.exports = function(grunt) {
       }
     },
 
+    uglify: {
+      js: {
+        files: {
+          "build/js/index.min.js": ["source/js/*.js"],
+          "build/js/catalog.min.js": ["source/js/menu.js", "source/js/modal.js", "source/js/picturefill.min.js"],
+          "build/js/form.min.js": ["source/js/menu.js", "source/js/picturefill.min.js"]
+        }
+      }
+    },
+
     browserSync: {
       server: {
         bsFiles: {
@@ -129,16 +184,20 @@ module.exports = function(grunt) {
     watch: {
       html: {
         files: ["source/*.html"],
-        tasks: ["posthtml"]
+        tasks: ["posthtml", "htmlmin"]
       },
       style: {
         files: ["source/less/**/*.less"],
         tasks: ["less", "postcss", "csso"]
+      },
+      js: {
+        files: ["source/js/**/*.js"],
+        tasks: ["uglify"]
       }
     }
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
 
-  grunt.registerTask("build", ["clean", "copy", "less", "postcss", "csso", "svgstore", "posthtml"]);
+  grunt.registerTask("build", ["clean", "copy", "less", "postcss", "csso", "svgstore", "posthtml", "htmlmin", "uglify"]);
 };
